d4258d282765f33f1333e39ac238ccbd57f7b8bc
Ryan Brennan <cryptichobbes257@gmail.com>
2021-10-20 11:25:04 -0700

    feat: Added Link functionality and isolated HTMl data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedPage = 'recipe';
+
+  showPage(link: string){
+    this.loadedPage = link;
+  }
 }
