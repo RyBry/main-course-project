3e4496dd79fb289f7d57baf1a71b9d2ed876a46d
Ryan Brennan <cryptichobbes257@gmail.com>
2021-10-21 16:42:33 -0700

    feat: Recipes can now be selected

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,8 +8,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeData: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
+
+onSelected(){
+  this.recipeSelected.emit();
+  console.log("show details emitter!");
+}
 
   ngOnInit(): void {
   }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe [] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.thespruceeats.com/thmb/cOrfL1MrSMQwKX9adyrFHtclObI=/1331x998/smart/filters:no_upscale()/slow-grilled-beef-ribs-335856-Hero-5b8c4b33c9e77c007bc64595.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.thespruceeats.com/thmb/cOrfL1MrSMQwKX9adyrFHtclObI=/1331x998/smart/filters:no_upscale()/slow-grilled-beef-ribs-335856-Hero-5b8c4b33c9e77c007bc64595.jpg'),
+    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://www.thespruceeats.com/thmb/cOrfL1MrSMQwKX9adyrFHtclObI=/1331x998/smart/filters:no_upscale()/slow-grilled-beef-ribs-335856-Hero-5b8c4b33c9e77c007bc64595.jpg'),
+    new Recipe('A Test Recipe2', 'This is simply a test2', 'https://www.thespruceeats.com/thmb/cOrfL1MrSMQwKX9adyrFHtclObI=/1331x998/smart/filters:no_upscale()/slow-grilled-beef-ribs-335856-Hero-5b8c4b33c9e77c007bc64595.jpg'),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
   
+  onRecipeSelected(recipeReceived: Recipe){
+    this.recipeWasSelected.emit(recipeReceived)
+    console.log(recipeReceived.name);
+  }
   ngOnInit(): void {
   }
 
